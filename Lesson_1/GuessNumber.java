69c421ebb0b376ed58c5d7b654db6055155a69ac
Ruben H <rooben86@gmail.com>
2022-01-05 10:20:19 +0300

    completed task GuessNumber

---
+++ Lesson_1/GuessNumber.java
@@ -0,0 +1,19 @@
+public class GuessNumber {
+    public static void main(String[] args) {
+
+int randomNumber = 0 +(int) (Math.random()*(100 + 1));
+System.out.println(randomNumber);
+int guess = 50;
+    while(guess != randomNumber) {
+        if(guess > randomNumber) {
+            System.out.println("Данное число больше того, что загадал компьютер");
+            guess--;
+        } else {
+            System.out.println("Данное число меньше того, что загадал компьютер");
+            guess++;
+        } 
+    } 
+    System.out.println("Поздравляю, число угадано! Это " + randomNumber);
+
+     }
+}
