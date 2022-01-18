e9d7a64693f57843f994b6a810e3ab23f0f06add
Ruben H <rooben86@gmail.com>
2022-01-18 21:14:51 +0400

    person class completed

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    char sex = 'm';
+    String name = "Ruben";
+    float height = 1.70f;
+    short weight = 73;
+    byte age = 35;
+
+    String walk() {
+        return "walking";
+    }
+
+    void sit() {
+        System.out.println("sitting");
+    }
+
+    String run() {
+        return "running";
+    }
+
+    void speak() {
+        System.out.println("speaking");
+    }
+
+    boolean learnJava() {
+        return true;
+    }
+}
