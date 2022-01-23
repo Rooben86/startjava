b38a214d5277b60365e322aa153c6b3a6979f964
Ruben H <rooben86@gmail.com>
2022-01-23 11:25:41 +0400

    added getters and setters to wolf and wolftest

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,37 +1,73 @@
 public class Wolf {
     
-    String sex;
-    String name;
-    int weight;
-    int age;
-    String color;
+    private String sex;
+    private String name;
+    private int weight;
+    private int age;
+    private String color;
 
-    public Wolf(String sex, String name, int weight, int age, String color) {
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if(age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
         this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
     }
-    
-    String walk() {
+
+    public String walk() {
         return "walking";
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("sitting");
     }
 
-    String run() {
+    public String run() {
         return "running";
     }
 
-    boolean howl() {
+    public boolean howl() {
         System.out.println("howling");
         return true;
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("hunting");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,13 +1,19 @@
 public class WolfTest {
     
     public static void main(String[] args) {
-        Wolf wolfOne = new Wolf("male", "Snoopy", 60, 3, "grey"); 
+        Wolf wolfOne = new Wolf();
+
+        wolfOne.setSex("female");
+        wolfOne.setName("Wendy");
+        wolfOne.setWeight(50);
+        wolfOne.setAge(7);
+        wolfOne.setColor("white");
         
-        System.out.println("This wolf is " + wolfOne.sex);
-        System.out.println("Its name is " + wolfOne.name);
-        System.out.println("Its weight is " + wolfOne.weight + " kilograms");
-        System.out.println("It's " + wolfOne.age + " years old");
-        System.out.println("Its color is " + wolfOne.color);
+        System.out.println("This wolf is " + wolfOne.getSex());
+        System.out.println("Its name is " + wolfOne.getName());
+        System.out.println("Its weight is " + wolfOne.getWeight() + " kilograms");
+        System.out.println("It's " + wolfOne.getAge() + " years old");
+        System.out.println("Its color is " + wolfOne.getColor());
 
         System.out.println(wolfOne.walk());
         wolfOne.sit();
