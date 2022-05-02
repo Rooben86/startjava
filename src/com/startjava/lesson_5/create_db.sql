d5979b1a5369441b84d1efe086d0b654c8b6ed5f
Ruben H <rooben86@gmail.com>
2022-05-02 10:04:20 +0300

    last fixes to hw5

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE Jaegers;
 \c jaegers;
 CREATE TABLE Jaegers (
-id SERIAL PRIMARY KEY,
-modelName TEXT,
-mark TEXT,
-height REAL,
-weight REAL,
-status TEXT,
-launch DATE,
-kaijuKill INTEGER)
-;
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark TEXT,
+    height REAL,
+    weight REAL,
+    status TEXT,
+    launch DATE,
+    kaijuKill INTEGER
+    );
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -4,6 +4,8 @@ SELECT * FROM Jaegers ORDER BY mark DESC;
 SELECT * FROM Jaegers WHERE mark = 'Mark-6' OR mark = 'Mark-1';
 SELECT * FROM Jaegers ORDER BY mark DESC;
 SELECT * FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+SELECT * FROM Jaegers WHERE kaijukill = (SELECT MAX(kaijukill) FROM Jaegers)
+OR kaijukill = (SELECT MIN(kaijukill) FROM Jaegers);
 SELECT AVG(weight) FROM Jaegers;
-UPDATE Jaegers SET kaijukill = kaijukill+1 WHERE status != 'Destroyed';
+UPDATE Jaegers SET kaijukill = kaijukill + 1 WHERE status != 'Destroyed';
 DELETE FROM Jaegers WHERE status = 'Destroyed';
